75d3c87edb0dab32bd305053f4cafd06a4541d44
Anton Hrlić <138965551+Anton-ai111@users.noreply.github.com>
2024-09-19 19:43:37 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = "My text that I want to display";
+            MessageBox.Show(text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 obj = new Form1();
+            if (obj == null)
+            {
+                obj.Parent = this;
+            }
+            obj.Show();
+            this.Hide();
+        }
+    }
+}
